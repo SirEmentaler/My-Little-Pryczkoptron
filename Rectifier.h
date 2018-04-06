d519ef4b419db5ba8cc5f3cd64de7634a78d21a8
SirEmentaler <sir.ementaler@gmail.com>
2018-04-06 11:46:26 +0200

    Add rectifier function

---
+++ Rectifier.h
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018 Jan Filipowicz, Filip Turobos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef RECTIFIER_H_
+#define RECTIFIER_H_
+
+#include "ActivationFunction.h"
+
+namespace mlp {
+
+/// Template class representing a linear activation function
+/**
+	Activation function @f$ f(x) = x^+ @f$ .
+
+	@tparam T Must meet the requirements of `NumericType`
+*/
+template<typename T>
+class Rectifier {
+public:
+	/// TODO
+	constexpr static T f(T x);
+	/// TODO
+	constexpr static T df(T);
+};
+
+/**
+	@param[in] x Function argument
+
+	@returns Function value @f$ f(x) = x^+ @f$
+*/
+template<typename T>
+constexpr T Rectifier<T>::f(T x) {
+	return std::max(T(), x);
+}
+
+/**
+	TODO
+*/
+template<typename T>
+constexpr T Rectifier<T>::df(T x) {
+	return x < T() ? T() : T(1);
+}
+
+}
+
+#endif
+
