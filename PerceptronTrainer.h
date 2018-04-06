fb65b245da2f2dd97efdff0803d30a5ff3d88e25
SirEmentaler <sir.ementaler@gmail.com>
2018-04-06 14:23:06 +0200

    Add perceptron trainer class

---
+++ PerceptronTrainer.h
@@ -0,0 +1,110 @@
+////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018 Jan Filipowicz, Filip Turobos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef PERCEPTRON_TRAINER_H_
+#define PERCEPTRON_TRAINER_H_
+
+#include <algorithm>
+#include <cstddef>
+#include <random>
+#include <utility>
+#include <vector>
+#include "MultiLayerPerceptron.h"
+#include "RandomNumberGenerator.h"
+
+namespace mlp {
+
+template<typename T>
+class PerceptronTrainer {
+public:
+	/// TODO
+	PerceptronTrainer(std::size_t inputSize, std::size_t outputSize);
+	/// TODO
+	template<class Perceptron>
+	void train(Perceptron& perceptron) const;
+	/// TODO
+	template<class InputIt1, class InputIt2>
+	void addTest(InputIt1 inFirst, InputIt2 outFirst);
+	/// TODO
+	void setMaxEpochs(std::size_t value) {maxEpochs = value;}
+	/// TODO
+	void setErrorThreshold(T value) {errorThreshold = value;}
+	/// TODO
+	void setLearningRate(T value) {learningRate = value;}
+	/// TODO
+	void setMomentum(T value) {momentum = value;}
+private:
+	std::vector<std::pair<std::vector<T>, std::vector<T>>> dataSet;
+	std::size_t inputSize;
+	std::size_t outputSize;
+	std::size_t maxEpochs;
+	T errorThreshold;
+	T learningRate;
+	T momentum;
+};
+
+/**
+	TODO
+*/
+template<typename T>
+PerceptronTrainer<T>::PerceptronTrainer(std::size_t inputSize, std::size_t outputSize)
+	: inputSize(inputSize), outputSize(outputSize) {}
+
+/**
+	TODO
+*/
+template<typename T>
+template<class Perceptron>
+void PerceptronTrainer<T>::train(Perceptron& perceptron) const {
+	double scaledThreshold = errorThreshold * dataSet.size();
+	RandomNumberGenerator<T, std::mt19937_64> generator(-1.0, 1.0);
+	perceptron.generateWeights(generator);
+	for (std::size_t i = maxEpochs; i--;) {
+		T error = T();
+		for (const auto& test : dataSet) {
+			error += perceptron.train(test.first.begin(), test.second.begin());
+		}
+		if (error < scaledThreshold)
+			return;
+		perceptron.apply(learningRate, momentum);
+	}
+}
+
+/**
+	TODO
+*/
+template<typename T>
+template<class InputIt1, class InputIt2>
+void PerceptronTrainer<T>::addTest(InputIt1 inFirst, InputIt2 outFirst) {
+	std::vector<T> in(inputSize);
+	std::copy_n(inFirst, inputSize, in.begin());
+	std::vector<T> out(outputSize);
+	std::copy_n(outFirst, outputSize, out.begin());
+	dataSet.emplace_back(std::move(in), std::move(out));
+}
+
+}
+
+#endif
+
--- main.cpp
+++ main.cpp
@@ -1,61 +1,37 @@
-#include <algorithm>
 #include <fstream>
-#include <iostream>
-#include <random>
 #include <utility>
+#include <vector>
 #include "IdentityFunction.h"
 #include "HyperbolicTangent.h"
 #include "MultiLayerPerceptron.h"
-#include "RandomNumberGenerator.h"
+#include "PerceptronTrainer.h"
 
 int main() {
 	mlp::MultiLayerPerceptron<double> network(1, {
 		{17, mlp::HyperbolicTangent<double>()},
 		{1, mlp::IdentityFunction<double>()},
 	});
-	mlp::RandomNumberGenerator<double, std::mt19937_64> generator(-1.0, 1.0);
-	network.generateWeights(generator);
-	{
-		std::vector<std::pair<double, double>> input;
-		std::ifstream in("approximation_train_1.txt");
-		for (std::pair<double, double> p; in >> p.first >> p.second;) {
-			input.push_back(p);
-		}
-		const int epochCount = 1000;
-		const int epochCountPercent = epochCount / 100;
-		std::mt19937_64 shuffler;
-		for (int i = 0; i < epochCount; i++) {
-			double error = 0.0;
-			std::shuffle(input.begin(), input.end(), shuffler);
-			for (const auto& p : input) {
-				error += network.train(&p.first, &p.second);
-			}
-			network.apply(1e-4, 0.99);
-			if (i % epochCountPercent == 0) {
-				std::cout << "Learning... " <<(i / epochCountPercent) << '%' << " - error is " << error << std::endl;
-			}
-		}
-		std::cout << "Learning complete" << std::endl;
+	mlp::PerceptronTrainer<double> trainer(1, 1);
+	trainer.setMaxEpochs(10000);
+	trainer.setErrorThreshold(0.06);
+	trainer.setLearningRate(1e-4);
+	trainer.setMomentum(0.9);
+	std::ifstream trainingData("approximation_train_1.txt");
+	for (double input, output; trainingData >> input >> output;) {
+		trainer.addTest(&input, &output);
 	}
-	{
-		std::vector<std::pair<double, double>> input;
-		std::ifstream in("approximation_test.txt");
-		for (std::pair<double, double> p; in >> p.first >> p.second;) {
-			input.push_back(p);
-		}
-		std::ofstream out("approximation_results_2.txt");
-		out << "Argument\tExpected\tObtained\n";
-		double error = 0.0;
-		std::sort(input.begin(), input.end());
-		std::cout << "Running tests..." << std::endl;
-		for (const auto& p : input) {
-			double output;
-			network.test(&p.first, &output);
-			error += (output - p.second) * (output - p.second);
-			out << p.first << '\t' << p.second << '\t' << output << '\n';
-		}
-		std::cout << "Done" << std::endl;
-		std::cout << "Average test error was " << (error / input.size()) << std::endl;
+	trainer.train(network);
+	std::vector<std::pair<double, double>> input;
+	std::ifstream in("approximation_test.txt");
+	for (std::pair<double, double> p; in >> p.first >> p.second;) {
+		input.push_back(p);
+	}
+	std::ofstream out("approximation_results_1.txt");
+	out << "Argument\tExpected\tObtained\n";
+	for (const auto& p : input) {
+		double output;
+		network.test(&p.first, &output);
+		out << p.first << '\t' << p.second << '\t' << output << '\n';
 	}
 	return 0;
 }
